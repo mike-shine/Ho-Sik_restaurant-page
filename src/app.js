07b2c5eb334b6cb7be8c8f551166b5905b6bbdb9
Mike Shine <michaelbshine@outlook.com>
2021-02-15 11:43:20 -0800

    added intro paragraphs, working on adding a font

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // const body = document.querySelector('body');
 const contentDiv = document.getElementById('content');
+const lineBreak = document.createElement('br');
 
 const header = document.createElement('div');
 header.classList.add('headerParent');
@@ -17,4 +18,30 @@ header.appendChild(h1);
 const dimSumPic = document.createElement('img');
 dimSumPic.src = '../public/dimSumPic.jpg';
 dimSumPic.classList.add('bodyItem');
+dimSumPic.style.width = '30%';
+dimSumPic.style.height = 'auto';
 bodySection.appendChild(dimSumPic);
+
+const introText = document.createElement('div')
+introText.classList.add('bodyItem', 'introTextParent');
+bodySection.appendChild(introText);
+
+const introHeader = document.createElement('h2');
+introHeader.classList.add('introTextItem');
+introHeader.textContent = 'Welcome to the website of Ho Sik Chinese Restaurant!';
+introHeader.style.fontStyle = 'italic';
+introText.appendChild(introHeader);
+
+const paragraph1 = document.createElement('p');
+paragraph1.classList.add('introTextItem');
+paragraph1.textContent = `We offer our customers delicious, authentically prepared southern Chinese cuisine at a reasonable price.`;
+introText.appendChild(paragraph1);
+
+introText.appendChild(lineBreak);
+
+const paragraph2 = document.createElement('p');
+paragraph2.classList.add('introTextItem');
+paragraph2.textContent = `Available for delivery on UberEats, GrubHub, PostMates, and Fantuan. For in-store pickup, please give us a call to place your order.`;
+introText.appendChild(paragraph2);
+
+

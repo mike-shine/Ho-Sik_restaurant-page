6b58030e11c039a5bce884e86d2bca4c7b89778c
Mike Shine <michaelbshine@outlook.com>
2021-02-15 10:40:51 -0800

    added image to new public folder, added image to DOM

--- src/app.js
+++ src/app.js
@@ -9,3 +9,7 @@ const h1 = document.createElement('h1');
 h1.classList.add('headerItem');
 h1.textContent = '好好食 中国餐厅 (Ho Sik Chinese Restaurant)';
 header.appendChild(h1);
+
+const dimSumPic = document.createElement('img');
+dimSumPic.src = '../public/dimSumPic.jpg';
+header.appendChild(dimSumPic);
